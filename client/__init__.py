55c34cdd0c5b10203443b577e0cda0fce3e6ce5d
BlockScience Labs <jupyter@blocksciencelabs.com>
2022-05-13 04:03:06 +0000

    Force reinstall with every fetch

--- client/__init__.py
+++ client/__init__.py
@@ -40,7 +40,7 @@ class Client:
         unserialized = []
         
         if response.status_code == 200:
-            subprocess.check_call([sys.executable, "-m", "pip", "install", get_wheel_url(simulation_id), "--user"])
+            subprocess.check_call([sys.executable, "-m", "pip", "install", "--user", "--ignore-installed", get_wheel_url(simulation_id)])
             for record in json.loads(response.text)["payload"]:
                 unserialized.append(pickle.loads(bytes.fromhex(record["data"])))
 
